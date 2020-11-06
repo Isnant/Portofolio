f848dee6f930b250b47d493f39c889588f8bd28c
isnan <isnantaufikkurrohman@gmail.com>
2020-11-06 10:55:47 +0700

    remove console

--- src/pages/equipment/js/migrationHistory.js
+++ src/pages/equipment/js/migrationHistory.js
@@ -153,7 +153,6 @@ export default {
         .then((response) => {
           this.$q.loading.hide()
           this.dataList = response.data.content
-          console.log(this.dataList)
         })
         .catch((error) => {
           this.$q.loading.hide()
