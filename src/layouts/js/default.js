2bd8e6c153a9cfa49f4e1d260c4a93fb09178460
isnan <isnantaufikkurrohman@gmail.com>
2021-09-14 21:00:51 +0700

    update user info

--- src/layouts/js/default.js
+++ src/layouts/js/default.js
@@ -23,42 +23,6 @@ export default {
     }
   },
   beforeMount () {
-    this.$q.loading.show()
-
-    const userToken = localStorage.getItem('user-token')
-
-    if ((userToken === undefined) || (userToken === null)) {
-      this.$q.loading.hide()
-      this.$router.push('/login')
-    } else {
-      this.userInfo = JSON.parse(JSON.parse(atob(userToken.split('.')[1])).userInfo)
-      const authorities = JSON.parse(atob(userToken.split('.')[1])).authorities
-      if (authorities.findIndex(x => x === 'ROLE_03') !== -1) {
-        this.menu.fieldEquipment = true
-      }
-      if (authorities.findIndex(x => x === 'ROLE_04') !== -1) {
-        this.menu.indoorEquipment = true
-      }
-      if (authorities.findIndex(x => x === 'ROLE_05') !== -1) {
-        this.menu.networkEquipmet = true
-      }
-      if (authorities.findIndex(x => x === 'ROLE_06') !== -1) {
-        this.menu.migrationHistory = true
-      }
-      if (authorities.findIndex(x => x === 'ROLE_07') !== -1) {
-        this.menu.fieldHierarchy = true
-      }
-      if (authorities.findIndex(x => x === 'ROLE_08') !== -1) {
-        this.menu.equipmentUploadLog = true
-      }
-      if (authorities.findIndex(x => x === 'ROLE_09') !== -1) {
-        this.menu.masterData = true
-      }
-      if (authorities.findIndex(x => x === 'ROLE_10') !== -1) {
-        this.menu.security = true
-      }
-      this.$q.loading.hide()
-    }
   },
   methods: {
     openURL
--- src/pages/equipment/js/main.js
+++ src/pages/equipment/js/main.js
@@ -27,32 +27,11 @@ export default {
 
   methods: {
     getInitPage () {
-      const userToken = localStorage.getItem('user-token')
-      this.userInfo = JSON.parse(JSON.parse(atob(userToken.split('.')[1])).userInfo)
-      if (this.userInfo.branch !== null) {
-        this.$axios.get(`${process.env.urlPrefix}getUserInfo`, {
-          params: {
-            id: this.userInfo.username
-          }
-        })
-          .then((response) => {
-            this.instance = response.data.user
-            this.instance.roles = this.instance.roles.substr(1, this.instance.roles.length - 2).split(';')
-            for (var prop in this.instance.roles) {
-              this.roles = this.roles.concat(response.data.listOfRoles.filter(v => v.value === this.instance.roles[prop]))
-            }
-            this.userInfo.branch = this.userInfo.branch.replace('["', '').replace('","', ', ').replace('"]', '')
-          })
-          .catch((error) => {
-            this.$q.loading.hide()
-            this.$q.notify({
-              color: 'negative',
-              icon: 'report_problem',
-              message: error
-            })
-          })
-      } else {
-        this.show.requestor = true
+      this.userInfo = {
+        username: 'Isnan',
+        fullName: 'Isnan Taufikkurrohman',
+        email: 'isnantaufikkurohman@gmail.com',
+        branch: 'Ciamis - Jawa Barat'
       }
     }
   }
